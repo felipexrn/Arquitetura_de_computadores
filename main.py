d6672c3fed936e06805ef8386b60bcd96f19430d
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-10-25 23:07:38 +0000

    tá oh... uma lindeza

--- main.py
+++ main.py
@@ -18,9 +18,14 @@ class Instrucao:
 nop = Instrucao("nop",0,0,0,0,0,0)
     
 def verifica_istrucao(instrucao):
+  
   instrucoes = ["add", "addi", "addiu", "addu", "and", "andi", "div", "divu", "mul", "mult", "multu", "nop", "nor", "or", "ori", "sll", "slt", "slti", "sltiu", "sltu", "sra", "srl", "sub", "subu", "syscall", "xor", "xori"]
   op_code = [0, 8, 9, 0, 0, 12, 0, 0, 28, 0, 0, 0, 0, 0, 13, 0, 0, 10, 11, 0, 0, 0, 0, 0, 0, 0, 14]
   func = [32,instrucao, instrucao, 33, 36, instrucao, 26, 27, 2, 24, 25, 0, 39, 37, instrucao, 0, 42, instrucao, instrucao, 43, 3, 2, 34, 35, 12, 38, instrucao]
+  if(instrucao[0] == instrucoes[6] or instrucao[0] == instrucoes[7] or instrucao[0] == instrucoes[9] or instrucao[0] == instrucoes[10]):
+    duplo = Instrucao(instrucao[0], op_code[instrucoes.index(instrucao[0])], rs, rt, rd,sa, function)
+    duplo + 0
+  
   for i in range(len(instrucoes)):
     if instrucao == instrucoes[i]:
       return [op_code[i], func[i]]
@@ -28,7 +33,7 @@ def verifica_istrucao(instrucao):
 while(True):
   try:
     instrucao = list(input().split())
-    inst = verifica_istrucao(instrucao[0])
+    inst = verifica_istrucao(instrucao)
     print(*inst)
   except:
     break
