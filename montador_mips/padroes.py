8123b50aef8eb14576db239a1eb07f09c7921eb2
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-10-31 23:39:36 +0000

    montador melhorado com dicionário de instruções

--- montador_mips/padroes.py
+++ montador_mips/padroes.py
@@ -1,258 +1,290 @@
-no = op = rs = rt = rd = sa = fu = 0
-_add = {
-  "nome": "add",
-  "op_code": 0,
-  "rs": "$",
-  "rt": "$",
-  "rd": "$",
-  "sa": 0,
-  "function": 32
-}
-_addi = {
-  "nome": "addi",
-  "op_code": 8,
-  "rs": "$",
-  "rt": "$",
-  "rd": 0,
-  "sa": 0,
-  "function": "imediato"
-}
-_addiu = {
-  "nome": "addiu",
-  "op_code": 9,
-  "rs": "$",
-  "rt": "$",
-  "rd": 0,
-  "sa": 0,
-  "function": "imediato"
-}
-_addu = {
-  "nome": "addu",
-  "op_code": 9,
-  "rs": "$",
-  "rt": "$",
-  "rd": "$",
-  "sa": 0,
-  "function": 33
-}
-_and = {
-  "nome": "and",
-  "op_code": 0,
-  "rs": "$",
-  "rt": "$",
-  "rd": "$",
-  "sa": 0,
-  "function": 36
-}
-_andi = {
-  "nome": "andi",
-  "op_code": 12,
-  "rs": "$",
-  "rt": "$",
-  "rd": 0,
-  "sa": 0,
-  "function": "imediato"
-}
-_div = {
-  "nome": "div",
-  "op_code": 0,
-  "rs": "$",
-  "rt": "$",
-  "rd": 0,
-  "sa": 0,
-  "function": 26
-}
-_divu = {
-  "nome": "divu",
-  "op_code": 0,
-  "rs": "$",
-  "rt": "$",
-  "rd": 0,
-  "sa": 0,
-  "function": 27
-}
-_mul = {
-  "nome": "mul",
-  "op_code": 28,
-  "rs": "$",
-  "rt": "$",
-  "rd": "$",
-  "sa": 0,
-  "function": 2
-}
-_mult = {
-  "nome": "mult",
-  "op_code": 0,
-  "rs": "$",
-  "rt": "$",
-  "rd": 0,
-  "sa": 0,
-  "function": 24
-}
-_multu = {
-  "nome": "multu",
-  "op_code": 0,
-  "rs": "$",
-  "rt": "$",
-  "rd": 0,
-  "sa": 0,
-  "function": 25
-}
-_nop = {
-  "nome": "multu",
-  "op_code": 0,
-  "rs": 0,
-  "rt": 0,
-  "rd": 0,
-  "sa": 0,
-  "function": 0
-}
-_nor = {
-  "nome": "nor",
-  "op_code": 0,
-  "rs": "$",
-  "rt": "$",
-  "rd": "$",
-  "sa": 0,
-  "function": 39
-}
-_or = {
-  "nome": "or",
-  "op_code": 0,
-  "rs": "$",
-  "rt": "$",
-  "rd": "$",
-  "sa": 0,
-  "function": 37
-}
-_ori = {
-  "nome": "ori",
-  "op_code": 13,
-  "rs": "$",
-  "rt": "$",
-  "rd": 0,
-  "sa": 0,
-  "function": "imediato"
-}
-_sll = {
-  "nome": "sll",
-  "op_code": 0,
-  "rs": 0,
-  "rt": "$",
-  "rd": "$",
-  "sa": "numero",
-  "function": 0
-}
-_slt = {
-  "nome": "slt",
-  "op_code": 0,
-  "rs": "$",
-  "rt": "$",
-  "rd": "$",
-  "sa": 0,
-  "function": 42
-}
-_slti = {
-  "nome": "slti",
-  "op_code": 10,
-  "rs": "$",
-  "rt": "$",
-  "rd": 0,
-  "sa": 0,
-  "function": "imediato"
-}
-_sltiu = {
-  "nome": "sltiu",
-  "op_code": 11,
-  "rs": "$",
-  "rt": "$",
-  "rd": 0,
-  "sa": 0,
-  "function": "imediato"
-}
-_sltu = {
-  "nome": "sltu",
-  "op_code": 0,
-  "rs": "$",
-  "rt": "$",
-  "rd": "$",
-  "sa": 0,
-  "function": 43
-}
-_sra = {
-  "nome": "sra",
-  "op_code": 0,
-  "rs": 0,
-  "rt": "$",
-  "rd": "$",
-  "sa": "numero",
-  "function": 3
-}
-_srl = {
-  "nome": "srl",
-  "op_code": 0,
-  "rs": 0,
-  "rt": "$",
-  "rd": "$",
-  "sa": "numero",
-  "function": 2
-}
-_sub = {
-  "nome": "sub",
-  "op_code": 0,
-  "rs": "$",
-  "rt": "$",
-  "rd": "$",
-  "sa": 0,
-  "function": 34
-}
-_subu = {
-  "nome": "subu",
-  "op_code": 0,
-  "rs": "$",
-  "rt": "$",
-  "rd": "$",
-  "sa": 0,
-  "function": 35
-}
-_syscall = {
-  "nome": "syscall",
-  "op_code": 0,
-  "rs": 0,
-  "rt": 0,
-  "rd": 0,
-  "sa": 0,
-  "function": 12
-}
-_xor = {
-  "nome": "xor",
-  "op_code": 0,
-  "rs": "$",
-  "rt": "$",
-  "rd": "$",
-  "sa": 0,
-  "function": 38
-}
-_xori = {
-  "nome": "xori",
-  "op_code": 14,
-  "rs": "$",
-  "rt": "$",
-  "rd": 0,
-  "sa": 0,
-  "function": "imediato"
-}
-instrucao = [_add, _addi, _addiu, _addu, _and, _andi, _div, _divu, _mul, _mult, _multu, _nop, _nor, _or, _ori, _sll, _slt, _slti, _sltiu, _sltu, _sra, _srl, _sub, _subu, _syscall, _xor, _xori]
+def get_padrao(nome_instrucao):
+  _add = {
+    "nome": "add",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": "$",
+    "sa": 0,
+    "function": 32,
+    "padrao": "R"
+  }
+  _addi = {
+    "nome": "addi",
+    "op_code": 8,
+    "rs": "$",
+    "rt": "$",
+    "rd": 0,
+    "sa": 0,
+    "function": "imediato",
+    "padrao": "I"
+  }
+  _addiu = {
+    "nome": "addiu",
+    "op_code": 9,
+    "rs": "$",
+    "rt": "$",
+    "rd": 0,
+    "sa": 0,
+    "function": "imediato",
+    "padrao": "I"
+  }
+  _addu = {
+    "nome": "addu",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": "$",
+    "sa": 0,
+    "function": 33,
+    "padrao": "R"
+  }
+  _and = {
+    "nome": "and",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": "$",
+    "sa": 0,
+    "function": 36,
+    "padrao": "R"
+  }
+  _andi = {
+    "nome": "andi",
+    "op_code": 12,
+    "rs": "$",
+    "rt": "$",
+    "rd": 0,
+    "sa": 0,
+    "function": "imediato",
+    "padrao": "I"
+  }
+  _div = {
+    "nome": "div",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": 0,
+    "sa": 0,
+    "function": 26,
+    "padrao": "Dupla"
+  }
+  _divu = {
+    "nome": "divu",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": 0,
+    "sa": 0,
+    "function": 27,
+    "padrao": "Dupla"
+  }
+  _mul = {
+    "nome": "mul",
+    "op_code": 28,
+    "rs": "$",
+    "rt": "$",
+    "rd": "$",
+    "sa": 0,
+    "function": 2,
+    "padrao": "R"
+  }
+  _mult = {
+    "nome": "mult",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": 0,
+    "sa": 0,
+    "function": 24,
+    "padrao": "Dupla"
+  }
+  _multu = {
+    "nome": "multu",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": 0,
+    "sa": 0,
+    "function": 25,
+    "padrao": "Dupla"
+  }
+  _nop = {
+    "nome": "nop",
+    "op_code": 0,
+    "rs": 0,
+    "rt": 0,
+    "rd": 0,
+    "sa": 0,
+    "function": 0,
+    "padrao": "R"
+  }
+  _nor = {
+    "nome": "nor",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": "$",
+    "sa": 0,
+    "function": 39,
+    "padrao": "R"
+  }
+  _or = {
+    "nome": "or",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": "$",
+    "sa": 0,
+    "function": 37,
+    "padrao": "R"
+  }
+  _ori = {
+    "nome": "ori",
+    "op_code": 13,
+    "rs": "$",
+    "rt": "$",
+    "rd": 0,
+    "sa": 0,
+    "function": "imediato",
+    "padrao": "I"
+  }
+  _sll = {
+    "nome": "sll",
+    "op_code": 0,
+    "rs": 0,
+    "rt": "$",
+    "rd": "$",
+    "sa": "numero",
+    "function": 0,
+    "padrao": "Sa"
+  }
+  _slt = {
+    "nome": "slt",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": "$",
+    "sa": 0,
+    "function": 42,
+    "padrao": "R"
+  }
+  _slti = {
+    "nome": "slti",
+    "op_code": 10,
+    "rs": "$",
+    "rt": "$",
+    "rd": 0,
+    "sa": 0,
+    "function": "imediato",
+    "padrao": "I"
+  }
+  _sltiu = {
+    "nome": "sltiu",
+    "op_code": 11,
+    "rs": "$",
+    "rt": "$",
+    "rd": 0,
+    "sa": 0,
+    "function": "imediato",
+    "padrao": "I"
+  }
+  _sltu = {
+    "nome": "sltu",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": "$",
+    "sa": 0,
+    "function": 43,
+    "padrao": "R"
+  }
+  _sra = {
+    "nome": "sra",
+    "op_code": 0,
+    "rs": 0,
+    "rt": "$",
+    "rd": "$",
+    "sa": "numero",
+    "function": 3,
+    "padrao": "Sa"
+  }
+  _srl = {
+    "nome": "srl",
+    "op_code": 0,
+    "rs": 0,
+    "rt": "$",
+    "rd": "$",
+    "sa": "numero",
+    "function": 2,
+    "padrao": "Sa"
+  }
+  _sub = {
+    "nome": "sub",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": "$",
+    "sa": 0,
+    "function": 34,
+    "padrao": "R"
+  }
+  _subu = {
+    "nome": "subu",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": "$",
+    "sa": 0,
+    "function": 35,
+    "padrao": "R"
+  }
+  _syscall = {
+    "nome": "syscall",
+    "op_code": 0,
+    "rs": 0,
+    "rt": 0,
+    "rd": 0,
+    "sa": 0,
+    "function": 12,
+    "padrao": "R"
+  }
+  _xor = {
+    "nome": "xor",
+    "op_code": 0,
+    "rs": "$",
+    "rt": "$",
+    "rd": "$",
+    "sa": 0,
+    "function": 38,
+    "padrao": "R"
+  }
+  _xori = {
+    "nome": "xori",
+    "op_code": 14,
+    "rs": "$",
+    "rt": "$",
+    "rd": 0,
+    "sa": 0,
+    "function": "imediato",
+    "padrao": "I"
+  }
 
-i = 0
-no = instrucao[i]["nome"]
-op = instrucao[i]["op_code"]
-rs = instrucao[i]["rs"]
-rt = instrucao[i]["rt"]
-rd = instrucao[i]["rd"]
-sa = instrucao[i]["sa"]
-fu = instrucao[i]["function"]
-
-
-print(no, op, rs, rt, rd, sa, fu)
+  instrucao = [_add, _addi, _addiu, _addu, _and, _andi, _div, _divu, _mul, _mult, _multu, _nop, _nor, _or, _ori, _sll, _slt, _slti, _sltiu, _sltu, _sra, _srl, _sub, _subu, _syscall, _xor, _xori]
+  
+  id = 0
+  for i in range(len(instrucao)):
+    if instrucao[i]["nome"] == nome_instrucao:
+      id = i
+      break
+  no = instrucao[id]["nome"]
+  op = instrucao[id]["op_code"]
+  rs = instrucao[id]["rs"]
+  rt = instrucao[id]["rt"]
+  rd = instrucao[id]["rd"]
+  sa = instrucao[id]["sa"]
+  fu = instrucao[id]["function"]
+  pa = instrucao[id]["padrao"] # R, I, Dupla, Sa
+  
+  return no, op, rs, rt, rd, sa, fu, pa
 
--- montador_mips/trivial.py
+++ montador_mips/trivial.py
@@ -1,12 +1,13 @@
+import padroes 
 # padrões encontrados nas 27 instruções da primeira etapa do projeto 
-# 11 instruções R [6,rs,rt,rd,5,6] ok
-# 7 instruções I [6,rs,rt,16] ok
-# 1 instrução nop 0 [6,5,5,5,5,6] ok
-# 1 instrução syscall [6,20,01100] ok
-# 4 instruções [6,rs,rt,10,6] ok
-# 3 instruções [6,5,rt,rd,sa,6] ok
-def dec_bin(op, rs, rt, rd, sa, fu, imediato):
-  if imediato:
+# 11 instruções R [6,rs,rt,rd,5,6]
+# 7 instruções I [6,rt,rs,16]
+# 1 instrução nop 0 [6,5,5,5,5,6]
+# 1 instrução syscall [6,5,5,5,5,6]
+# 4 instruções Dupla [6,rs,rt,5,5,6]
+# 3 instruções Sa [6,5,rt,rd,sa,6]
+def dec_bin(op, rs, rt, rd, sa, fu, pa):
+  if pa == "I":
     binario = "{:06b}".format(op)
     binario += "{:05b}".format(rs)
     binario += "{:05b}".format(rt)
@@ -19,6 +20,7 @@ def dec_bin(op, rs, rt, rd, sa, fu, imediato):
     binario += "{:05b}".format(sa)
     binario += "{:06b}".format(fu)
   return binario
+  
 def bin_hex(binario):
   hexadecimal = ""
   bin = binario
@@ -32,80 +34,64 @@ def bin_hex(binario):
     hexadecimal += "{:x}".format(dec)
     bin = bin[4:]
   return hexadecimal
+  
 def monta_instrucao(instrucao):
-  nome = ["add", "addi", "addiu", "addu", "and", "andi", "div", "divu", "mul", "mult", "multu", "nop", "nor", "or", "ori", "sll", "slt", "slti", "sltiu", "sltu", "sra", "srl", "sub", "subu", "syscall", "xor", "xori"]
-  op_code = [0, 8, 9, 0, 0, 12, 0, 0, 28, 0, 0, 0, 0, 0, 13, 0, 0, 10, 11, 0, 0, 0, 0, 0, 0, 0, 14]
-  function = [32,"imediato", "imediato", 33, 36, "imediato", 26, 27, 2, 24, 25, 0, 39, 37, "imediato", 0, 42, "imediato", "imediato", 43, 3, 2, 34, 35, 12, 38, "imediato"]
-  id_nome = nome.index(instrucao[0])
-  imediato = False
-  try:
-    tem_i = instrucao[0].index('i') > 0
-  except:
-    tem_i = False
-  if instrucao[0] == nome[6] or instrucao[0] == nome[7] or instrucao[0] == nome[9] or instrucao[0] == nome[10]: # duplas
-    instrucao[1] = int(instrucao[1][1:])
-    instrucao[2] = int(instrucao[2][1:])
-    no = nome[id_nome]
-    op = op_code[id_nome]
-    rs = instrucao[1]
-    rt = instrucao[2]
+  no, op, rs, rt, rd, sa, fu, pa = padroes.get_padrao(instrucao[0])
+  if pa == "Dupla": # Dois registradores
+    no = no
+    op = op
+    rs = int(instrucao[1])
+    rt = int(instrucao[2])
     rd = 0
     sa = 0
-    fu = function[id_nome]
-  elif instrucao[0] == nome[11]: # nop
-    no = nome[id_nome]
+    fu = fu
+  elif no == "nop": # nop
+    no = no
     op = 0
     rs = 0
     rt = 0
     rd = 0
     sa = 0
     fu = 0
-  elif instrucao[0] == nome[24]: # syscall
-    no = nome[id_nome]
-    op = op_code[id_nome]
+  elif no == "syscall": # syscall
+    no = no
+    op = op
     rs = 0
     rt = 0
     rd = 0
     sa = 0
-    fu = function[id_nome]
-  elif instrucao[0] == nome[21] or instrucao[0] == nome[20] or instrucao[0] == nome[15]: # sa
-    instrucao[1] = int(instrucao[1][1:])
-    instrucao[2] = int(instrucao[2][1:])
-    no = nome[id_nome]
-    op = op_code[id_nome]
+    fu = fu
+  elif pa == "Sa": # sa
+    no = no
+    op = op
     rs = 0
-    rt = instrucao[2]
-    rd = instrucao[1]
+    rt = int(instrucao[2])
+    rd = int(instrucao[1])
     sa = int(instrucao[3])
-    fu = function[id_nome]
-  elif instrucao[0][0] != 'd' and tem_i: # padrão I
-    imediato = True
-    instrucao[1] = int(instrucao[1][1:])
-    instrucao[2] = int(instrucao[2][1:])
-    no = nome[id_nome]
-    op = op_code[id_nome]
-    rs = instrucao[2]
-    rt = instrucao[1]
+    fu = fu
+  elif pa == "I": # padrão I
+    no = no
+    op = op
+    rs = int(instrucao[2])
+    rt = int(instrucao[1])
     rd = 0
     sa = 0
     fu = int(instrucao[3])    
-  else: # padrão R
-    instrucao[1] = int(instrucao[1][1:])
-    instrucao[2] = int(instrucao[2][1:])
-    instrucao[3] = int(instrucao[3][1:])
-    no = nome[id_nome]
-    op = op_code[id_nome]
-    rs = instrucao[2]
-    rt = instrucao[3]
-    rd = instrucao[1]
+  elif pa == "R": # padrão R
+    no = no
+    op = op
+    rs = int(instrucao[2])
+    rt = int(instrucao[3])
+    rd = int(instrucao[1])
     sa = 0
-    fu = function[id_nome]
-  return no, op, rs, rt, rd, sa, fu, imediato
+    fu = fu
+  return no, op, rs, rt, rd, sa, fu, pa
+
 while(True):
   try:
-    instrucao = list(input().split())
-    no, op, rs, rt, rd, sa, fu, imediato = monta_instrucao(instrucao)
-    binario = dec_bin(op, rs, rt, rd, sa, fu, imediato)
+    instrucao = list(input().replace("$", "").split())
+    no, op, rs, rt, rd, sa, fu, pa = monta_instrucao(instrucao)
+    binario = dec_bin(op, rs, rt, rd, sa, fu, pa)
     hexadecimal = bin_hex(binario)
     print(hexadecimal)
   except:
