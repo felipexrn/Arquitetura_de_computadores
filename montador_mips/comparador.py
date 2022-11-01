97625e524c05879a2b53fd99bd8453820c2b5e4b
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-11-01 02:41:42 +0000

    correção comparador

--- montador_mips/comparador.py
+++ montador_mips/comparador.py
@@ -1,24 +1,23 @@
 linha = 0
 correto = 0
-errado = 0
 mips = open("traducao_mips.txt")
 montador = open("traducao_montador.txt")
-while (linha < 27):
-  try:
-    linha += 1
-    base = mips.readline().rstrip()
-    teste = montador.readline().rstrip()
-    if linha < 10:
-      zero = "0"
-    else:
-      zero = ""      
-    if base == teste:
-      print(f"Linha {zero}{linha} está correta: {base}")
-      correto += 1
-    else:
-      print(f"Linha {linha} está ERRADA: {base} != {teste}")
-  except:
+while (True):
+  base = mips.readline().rstrip()
+  teste = montador.readline().rstrip()
+  if base == "" and teste == "":
     break
+  else:
+    linha += 1
+  if linha < 10:
+    zero = "0"
+  else:
+    zero = ""      
+  if base == teste:
+    print(f"Linha {zero}{linha} está correta: {base}")
+    correto += 1
+  else:
+    print(f"Linha {linha} está ERRADA: {base} != {teste}")
 mips.close()
 montador.close()
 print(str(int(correto/linha*100))+"% de acerto")
