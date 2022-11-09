eb9b0254d4a4f561e961f723b2d6db03c840cb38
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-11-09 08:38:24 +0000

    lista_02.10.asm resolvida

---
+++ assembly/lista_02/lista_02.10.asm
@@ -0,0 +1,30 @@
+.text
+main:	addi $v0 $0 5
+	syscall
+	add $t0 $0 $v0
+	addi $v0 $0 5
+	syscall
+	add $t1 $0 $v0
+	addi $v0 $0 5
+	syscall
+	add $t2 $0 $v0
+	add $a0 $t0 $t1
+	add $a0 $a0 $t2
+	addi $t3 $0 3
+	div $a0 $t3
+	mflo $a0
+	mfhi $t4
+	addi $t3 $0 1
+	slt $t5 $t3 $t4
+	add $a0 $a0 $t5
+	addi $v0 $0 1
+	syscall
+	add $a0 $0 ','
+	addi $v0 $0 11
+	syscall
+	add $a0 $0 $0
+	addi $v0 $0 1
+	syscall
+fim:	addi $v0 $0 10
+	syscall
+	
