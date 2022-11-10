5224b88b53aeb74c152df6db86851625ec00fe19
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-11-10 12:27:30 +0000

    lista_02.15.asm em desenvolvimento

---
+++ assembly/lista_02/lista_02.15.asm
@@ -0,0 +1,13 @@
+#Dia
+#Mes
+#Ano
+#A = Mes + 1
+#B = A / 5
+#C = B * 3
+#D = Mes * 2
+#E = Ano / 4
+#F = Ano / 100
+#G = Ano / 400
+#H = Dia + D + C + Ano + E - F + G + 2
+#Semana = H % 7
+#Semana = (Dia + 2 * Mes + (3 * (Mes + 1) / 5) + Ano + Ano / 4 - Ano / 100 + Ano / 400 + 2) % 7;
