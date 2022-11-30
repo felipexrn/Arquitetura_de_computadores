cc927ebf2438e31438ff66f72db642d00b1aecb3
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-11-30 19:41:22 +0000

    lista_03_04.asm resolvida

--- assembly/lista_03/lista_03_04.asm
+++ assembly/lista_03/lista_03_04.asm
@@ -1,5 +1,5 @@
-# Faça um programa que leia do usuário um intervalo fechado
-# e imprima os números pares desse intervalo (inclusive os limites).
+# Programa que leia do usuario um intervalo fechado
+# e imprima os numeros pares desse intervalo (inclusive os limites).
 .text
 main: 	addi $v0 $0 5
 	syscall
@@ -7,6 +7,20 @@ main: 	addi $v0 $0 5
 	addi $v0 $0 5
 	syscall
 	add $t1 $0 $v0 # fim
-
-for:	
+	sub $t2 $t1 $t0 # intervalo
+	addi $t3 $0 0 # incremento
 	
+for:	add $a0 $t0 $t3
+	andi $v0 $a0 1
+	bne $v0 $0 rfor
+prn:	addi $v0 $0 1
+	syscall
+	addi $a0 $0 '\n'
+	addi $v0 $0 11
+	syscall
+rfor:	beq $t2 $t3 end
+	addi $t3 $t3 1
+	j for
+
+end:	addi $v0 $0 10
+	syscall
