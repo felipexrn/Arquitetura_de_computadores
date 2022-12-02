4bd554c4f6e4bde68e40912da71c72ec52ac284b
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-12-02 00:07:25 +0000

    lista_03_08.asm resolvida

--- assembly/lista_03/lista_03_08.asm
+++ assembly/lista_03/lista_03_08.asm
@@ -1,23 +1,25 @@
-# Faça um programa que leia números inteiros diferentes de zero
+# Faca um programa que leia nÃ¯Â¿Â½meros inteiros diferentes de zero
 # e encontre o menor valor digitado e o maior valor digitado.
 # O programa informa o maior e o menor, um em cada linha,
-# quando o usuário digitar um 0.
+# quando o usuario digitar um 0.
 .text
-main:	addi $t0 $0 0 # maior
-	addi $t1 $0 0 # menor
-	
-while:	addi $v0 $0 5
+main:	addi $v0 $0 5
 	syscall
-	beq $v0 $0 prn
+	add $t0 $0 $v0 # maior
+	add $t1 $0 $v0 # menor
 	
-maior:	slt $t2 $v0 $t0 # $t0 >= $v0? 1:0
-	beq $t0 $0 menor
+maior:	slt $t2 $t0 $v0 # $v0 >= $t0? 1:0
+	beq $t2 $0 menor
 	add $t0 $0 $v0
 	
 menor:	slt $t2 $v0 $t1 # $v0 < $t1? 1:0
-	beq $t1 $0 while
+	beq $t2 $0 while
 	add $t1 $0 $v0
-	j while
+	
+while:	addi $v0 $0 5 # do while
+	syscall
+	beq $v0 $0 prn
+	j maior
 	
 prn:	add $a0 $0 $t0
 	addi $v0 $0 1
