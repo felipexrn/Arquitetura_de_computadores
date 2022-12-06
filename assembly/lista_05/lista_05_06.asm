0052acc2fae32e2f864c6f40403ef3516f365dc5
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-12-06 17:18:24 +0000

    lista_05_06.asm resolvida

--- assembly/lista_05/lista_05_06.asm
+++ assembly/lista_05/lista_05_06.asm
@@ -6,7 +6,7 @@ main:	lui $t0 0x1001 # aponta para v[0]
 	addi $t1 $0 6 # t = 6
 	add $t2 $0 0 # contador = 0
 	
-for:	beq $t1 $t2 prn # $t1 == $t2? prn: prox instr
+for:	beq $t1 $t2 v # $t1 == $t2? prn: prox instr
 	addi $v0 $0 5
 	syscall
 	
@@ -14,7 +14,22 @@ par:	andi $t3 $v0 1 # par? 0:1
 	bne $t3 $0 for
 	sw $v0 0($t0) # v[i] = entrada
 	addi $t2 $t2 1 # contator ++
-	addi $t0 $t0 4 # aponta para v[i]
+	addi $t0 $t0 4 # aponta para v[i]+1
 	j for
 	
-prn:	
+v:	addi $t0 $t0 -4 # aponta para v[5]
+	
+prn:	beq $t1 $0 end # t == 0? end: prox instr
+	lw $a0 0($t0) # $a0 = v[i]
+	addi $v0 $0 1
+	syscall 	# imprime v[i]
+	addi $a0 $0 ' '
+	addi $v0 $0 11 
+	syscall		# imprime ' '
+	addi $t0 $t0 -4 # aponta para v[i]-1
+	addi $t1 $t1 -1 # t--
+	j prn
+	
+end:	addi $v0 $0 10
+	syscall
+	
