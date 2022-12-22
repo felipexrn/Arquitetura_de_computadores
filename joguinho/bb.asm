2449ff04b97fc859a2e112669781055c3c1ef3cf
felipe.xrn <112009958+felipexrn@users.noreply.github.com>
2022-12-22 18:56:01 +0000

    atualizações tela

--- joguinho/bb.asm
+++ joguinho/bb.asm
@@ -322,181 +322,207 @@
 .text
 main: nop
 
-# cenarios
-addi $a1 $0 0 # 1 mapa de bits. 0 funcoes
-beq $a1 $0 retangulos
+# propriedades graficas
+	addi $s0 $0 512 # width
+	addi $s1 $0 256 # height
+	addi $s2 $0 4 # proporcao
+	div $s0 $s2
+	mflo $s3 # w
+	div $s1 $s2
+	mflo $s4 # h
 
-addi $a0 $0 1 # numero cenário a ser carregado
+# cenarios
+	addi $a1 $0 0 # 1 mapa de bits. 0 funcoes
+	beq $a1 $0 retangulos
+
+	addi $a0 $0 1 # numero cenario a ser carregado
 
 loadscream:
-sll $a0 $a0 2 # byte de memoria
-addi $a1 $0 8192 # tamanho da tela
-mul $a0 $a0 $a1 # offset de memoria ate o cenario
-addi $a0 $a0 0x10010000 # endereco inicial do cenario
-lui $a2 0x1001 # inicio da escrita
+	sll $a0 $a0 2 # byte de memoria
+	mul $a1 $s3 $s4 # tamanho da tela
+	mul $a0 $a0 $a1 # offset de memoria ate o cenario
+	addi $a0 $a0 0x10010000 # endereco inicial do cenario
+	lui $a2 0x1001 # inicio da escrita
 
 loadpix:
-beq $a1 $0 fimloadpix
-lw $a3 0($a0) # copia cenario
-sw $a3 0($a2) # escreve cenario
-addi $a0 $a0 4 # prx pixel a ser copiado
-addi $a2 $a2 4 # prx pixel a ser escrito
-addi $a1 $a1 -1 # variavel de iteracao
-j loadpix
+	beq $a1 $0 fimloadpix
+	lw $a3 0($a0) # copia cenario
+	sw $a3 0($a2) # escreve cenario
+	addi $a0 $a0 4 # prx pixel a ser copiado
+	addi $a2 $a2 4 # prx pixel a ser escrito
+	addi $a1 $a1 -1 # variavel de iteracao
+	j loadpix
 fimloadpix:
 
-j end
+	j end
 
-j retangulos
+	j retangulos
 
 # circulos
 
 circulos:
 
-addi $t0 $0 7 # raio
-add $t1 $0 $t0 
-addi $t2 $0 3 # pi
-addi $a0 $0 0x10010000 # inicio
-addi $t4 $0 67 # x
-sll $t4 $t4 2
-addi $t5 $0 9 # y
-sll $t5 $t5 9
-add $a0 $a0 $t4
-add $a0 $a0 $t5 
-add $t6 $0 $a0 # inicio
-addi $a1 $0 0xffff00 # cor
+sol:	addi $t0 $0 7 # raio
+	add $t1 $0 $t0 
+	addi $t2 $0 3 # pi
+	addi $a0 $0 0x10010000 # inicio
+	addi $t4 $0 67 # x
+	sll $t4 $t4 2
+	addi $t5 $0 9 # y
+	sll $t5 $t5 9
+	add $a0 $a0 $t4
+	add $a0 $a0 $t5 
+	add $t6 $0 $a0 # inicio
+	addi $a1 $0 0xffff00 # cor
 
-jal circ
+	jal circ
 
-j end
+marte:
+
+	addi $t0 $0 3 # raio
+	add $t1 $0 $t0 
+	addi $t2 $0 3 # pi
+	addi $a0 $0 0x10010000 # inicio
+	addi $t4 $0 15 # x
+	sll $t4 $t4 2
+	addi $t5 $0 32 # y
+	sll $t5 $t5 9
+	add $a0 $a0 $t4
+	add $a0 $a0 $t5 
+	add $t6 $0 $a0 # inicio
+	addi $a1 $0 0xff0000 # cor
+
+	jal circ
+
+	j end
 
 circ:
-sw $ra 0($sp)
-addi $sp $sp -4
+	sw $ra 0($sp)
+	addi $sp $sp -4
 
-q4: beq $t1 $0 fimq4
+q4: 
+	beq $t1 $0 fimq4
+	sub $t3 $t0 $t1 # A = raio - linha
+	sll $t3 $t3 1 # B = A * 2
+	div $t3 $t2 # C = B MOD 3
+	mflo $t3
+	mul $t3 $t3 $t3 # D = C**2
+	srl $t3 $t3 2 # E = D MOD 4
 
-sub $t3 $t0 $t1 # A = raio - linha
-sll $t3 $t3 1 # B = A * 2
-div $t3 $t2 # C = B MOD 3
-mflo $t3
-mul $t3 $t3 $t3 # D = C**2
-srl $t3 $t3 2 # E = D MOD 4
+	sub $a2 $t0 $t3 # tamanho
 
-sub $a2 $t0 $t3 # tamanho
+	jal linhap
 
-jal linhap
-
-addi $t6 $t6 512
-sub $a2 $t0 $t3
-sll $a2 $a2 2
-sub $t6 $t6 $a2
-addi $t1 $t1 -1
-j q4
+	addi $t6 $t6 512
+	sub $a2 $t0 $t3
+	sll $a2 $a2 2
+	sub $t6 $t6 $a2
+	addi $t1 $t1 -1
+	j q4
 
 fimq4: 
-add $t1 $0 $t0
-add $t6 $0 $a0 # inicio
-q1: beq $t1 $0 fimq1
+	add $t1 $0 $t0
+	add $t6 $0 $a0 # inicio
+q1: 
+	beq $t1 $0 fimq1
+	sub $t3 $t0 $t1 # A = raio - linha
+	sll $t3 $t3 1 # B = A * 2
+	div $t3 $t2 # C = B MOD 3
+	mflo $t3
+	mul $t3 $t3 $t3 # D = C**2
+	srl $t3 $t3 2 # E = D MOD 4
 
-sub $t3 $t0 $t1 # A = raio - linha
-sll $t3 $t3 1 # B = A * 2
-div $t3 $t2 # C = B MOD 3
-mflo $t3
-mul $t3 $t3 $t3 # D = C**2
-srl $t3 $t3 2 # E = D MOD 4
+	sub $a2 $t0 $t3 # tamanho
 
-sub $a2 $t0 $t3 # tamanho
+	jal linhap
 
-jal linhap
-
-addi $t6 $t6 -512
-sub $a2 $t0 $t3
-sll $a2 $a2 2
-sub $t6 $t6 $a2
-addi $t1 $t1 -1
-j q1
+	addi $t6 $t6 -512
+	sub $a2 $t0 $t3
+	sll $a2 $a2 2
+	sub $t6 $t6 $a2
+	addi $t1 $t1 -1
+	j q1
 
 fimq1:
-add $t1 $0 $t0
-add $t6 $0 $a0 # inicio
+	add $t1 $0 $t0
+	add $t6 $0 $a0 # inicio
 
-q2: beq $t1 $0 fimq2
+q2:
+	beq $t1 $0 fimq2
+	sub $t3 $t0 $t1 # A = raio - linha
+	sll $t3 $t3 1 # B = A * 2
+	div $t3 $t2 # C = B MOD 3
+	mflo $t3
+	mul $t3 $t3 $t3 # D = C**2
+	srl $t3 $t3 2 # E = D MOD 4
 
-sub $t3 $t0 $t1 # A = raio - linha
-sll $t3 $t3 1 # B = A * 2
-div $t3 $t2 # C = B MOD 3
-mflo $t3
-mul $t3 $t3 $t3 # D = C**2
-srl $t3 $t3 2 # E = D MOD 4
+	sub $a2 $t0 $t3 # tamanho
 
-sub $a2 $t0 $t3 # tamanho
+	jal linhan
 
-jal linhan
-
-addi $t6 $t6 -512
-sub $a2 $t0 $t3
-sll $a2 $a2 2
-add $t6 $t6 $a2
-addi $t1 $t1 -1
-j q2
+	addi $t6 $t6 -512
+	sub $a2 $t0 $t3
+	sll $a2 $a2 2
+	add $t6 $t6 $a2
+	addi $t1 $t1 -1
+	j q2
 
 fimq2:
-add $t1 $0 $t0
-add $t6 $0 $a0 # inicio
+	add $t1 $0 $t0
+	add $t6 $0 $a0 # inicio
 
-q3: beq $t1 $0 fimq3
+q3:
+	beq $t1 $0 fimq3
+	sub $t3 $t0 $t1 # A = raio - linha
+	sll $t3 $t3 1 # B = A * 2
+	div $t3 $t2 # C = B MOD 3
+	mflo $t3
+	mul $t3 $t3 $t3 # D = C**2
+	srl $t3 $t3 2 # E = D MOD 4
 
-sub $t3 $t0 $t1 # A = raio - linha
-sll $t3 $t3 1 # B = A * 2
-div $t3 $t2 # C = B MOD 3
-mflo $t3
-mul $t3 $t3 $t3 # D = C**2
-srl $t3 $t3 2 # E = D MOD 4
+	sub $a2 $t0 $t3 # tamanho
 
-sub $a2 $t0 $t3 # tamanho
+	jal linhan
 
-jal linhan
-
-addi $t6 $t6 512
-sub $a2 $t0 $t3
-sll $a2 $a2 2
-add $t6 $t6 $a2
-addi $t1 $t1 -1
-j q3
+	addi $t6 $t6 512
+	sub $a2 $t0 $t3
+	sll $a2 $a2 2
+	add $t6 $t6 $a2
+	addi $t1 $t1 -1
+	j q3
 
 fimq3:
-addi $sp $sp 4
-lw $ra 0($sp)
-jr $ra
+	addi $sp $sp 4
+	lw $ra 0($sp)
+	jr $ra
 
 linhap:
-sw $ra 0($sp)
-addi $sp $sp -4
+	sw $ra 0($sp)
+	addi $sp $sp -4
 lp:
-beq $a2 $0 fimlp
-sw $a1 0($t6)
-addi $t6 $t6 4
-addi $a2 $a2 -1
-j lp
+	beq $a2 $0 fimlp
+	sw $a1 0($t6)
+	addi $t6 $t6 4
+	addi $a2 $a2 -1
+	j lp
 fimlp: 
-addi $sp $sp 4
-lw $ra 0($sp)
-jr $ra
+	addi $sp $sp 4
+	lw $ra 0($sp)
+	jr $ra
 
 linhan:
-sw $ra 0($sp)
-addi $sp $sp -4
+	sw $ra 0($sp)
+	addi $sp $sp -4
 ln:
-beq $a2 $0 fimln
-sw $a1 0($t6)
-addi $t6 $t6 -4
-addi $a2 $a2 -1
-j ln
+	beq $a2 $0 fimln
+	sw $a1 0($t6)
+	addi $t6 $t6 -4
+	addi $a2 $a2 -1
+	j ln
 fimln: 
-addi $sp $sp 4
-lw $ra 0($sp)
-jr $ra
+	addi $sp $sp 4
+	lw $ra 0($sp)
+	jr $ra
 
 
 # retangulos
@@ -504,158 +530,190 @@ jr $ra
 retangulos:
 
 ceu:
-addi $a0 $0 0x10010000 # endereco
-addi $a1 $0 0x78d4e7 # RGB 0x ff ff ff
-addi $a2 $0 128 # b
-addi $a3 $0 64 # h
-addi $t0 $0 0 # x
-addi $t1 $0 0 # y
+	addi $a0 $0 0x10010000 # endereco
+	addi $a1 $0 0x78d4f9 # RGB 0x ff ff ff cor do ceu
+	addi $a2 $0 128 # b
+	addi $a3 $0 64 # h
+	addi $t0 $0 0 # x
+	addi $t1 $0 0 # y
 	
-jal ret
+	jal ret
 
 # desenha o chao
-	
-addi $a0 $0 0x10010000 # endereco
-addi $a1 $0 0xe7c278 # RGB 0x ff ff ff
-addi $a2 $0 55 # b
-addi $a3 $0 5 # h
-addi $t0 $0 0 # x
-addi $t1 $0 59 # y
-	
-jal chao
 
-addi $a0 $0 0x10010000 # endereco
-addi $a1 $0 0xe7c278 # RGB 0x ff ff ff
-addi $a2 $0 55 # b
-addi $a3 $0 5 # h
-addi $t0 $0 65 # x
-addi $t1 $0 59 # y
+terreno:
+	addi $a0 $0 0x10010000 # endereco
+	addi $a2 $0 55 # b
+	addi $a3 $0 5 # h
+	addi $t0 $0 0 # x
+	addi $t1 $0 59 # y
 	
-jal chao
-	
-addi $a0 $0 0x10010000 # endereco
-addi $a1 $0 0xe7c278 # RGB 0x ff ff ff
-addi $a2 $0 10 # b
-addi $a3 $0 5 # h
-addi $t0 $0 0 # x
-addi $t1 $0 54 # y
-	
-jal chao
+	jal chao
 
-j nuvem
+	addi $a0 $0 0x10010000 # endereco
+	addi $a2 $0 55 # b
+	addi $a3 $0 5 # h
+	addi $t0 $0 65 # x
+	addi $t1 $0 59 # y
+	
+	jal chao
+	
+	addi $a0 $0 0x10010000 # endereco
+	addi $a2 $0 10 # b
+	addi $a3 $0 5 # h
+	addi $t0 $0 0 # x
+	addi $t1 $0 54 # y
+	
+	jal chao
+	
+	addi $a0 $0 0x10010000 # endereco
+	addi $a2 $0 10 # b
+	addi $a3 $0 5 # h
+	addi $t0 $0 83 # x
+	addi $t1 $0 54 # y
+	
+	jal chao
+	
+	addi $a0 $0 0x10010000 # endereco
+	addi $a2 $0 35 # b
+	addi $a3 $0 4 # h
+	addi $t0 $0 40 # x
+	addi $t1 $0 20 # y
+	
+	jal chao
+	
+	addi $t0 $0 40 # x
+	addi $t1 $0 9 # y
+
+	jal nuvem
+	
+	j circulos
 
 chao:	
-sw $ra 0($sp)
-addi $sp $sp -4
+	sw $ra 0($sp)
+	addi $sp $sp -4
 	
-add $t2 $0 $a2 # b grama
-add $t4 $0 $t0 # x grama
-add $t5 $0 $t1 # y grama
+	add $t2 $0 $a2 # b grama
+	add $t4 $0 $t0 # x grama
+	add $t5 $0 $t1 # y grama
 	
-jal ret
+	addi $a1 $0 0xe7c278 # RGB 0x ff ff ff cor do chão
 	
-addi $a0 $0 0x10010000 # endereco
-addi $a1 $0 0x42d690 # RGB 0x ff ff ff
-add $a2 $0 $t2 # b
-addi $a3 $0 1 # h
-add $t0 $0 $t4 # x
-add $t1 $0 $t5 # y
+	jal ret
 	
-jal ret	
+	addi $a0 $0 0x10010000 # endereco
+	addi $a1 $0 0x21d648 # RGB 0x ff ff ff cor da grama
+	add $a2 $0 $t2 # b
+	addi $a3 $0 1 # h
+	add $t0 $0 $t4 # x
+	add $t1 $0 $t5 # y
+	
+	jal ret	
 
-addi $sp $sp 4
-lw $ra 0($sp)
-jr $ra
+	addi $sp $sp 4
+	lw $ra 0($sp)
+	jr $ra
 
 # desenha nuvem com funcao
 nuvem:
+	
+	sw $ra 0($sp)
+	addi $sp $sp -4
+	
+	add $t5 $0 $t0 # x # 40
+	add $t6 $0 $t1 # y # 9
+	#addi $a0 $0 0x10010000 # endereco
+	
+	addi $a0 $0 0x10010000 # endereco
+	add $t0 $0 $t5 # x
+	add $t1 $0 $t6 # y
+	addi $a1 $0 0xffffff # cor
+	addi $a2 $0 7 # b
+	add $a3 $0 2 # h
+	
+	jal ret
 
-addi $a0 $0 0x10010000 # endereco
-addi $t0 $0 50 # x
-addi $t1 $0 10 # y
-addi $a1 $0 0xffffff # cor
-addi $a2 $0 6 # b
-add $a3 $0 3 # h
+	addi $a0 $0 0x10010000 # endereco
+	addi $t0 $t5 10 # x
+	addi $t1 $t6 1 # y
+	addi $a1 $0 0xffffff # cor
+	addi $a2 $0 6 # b
+	add $a3 $0 3 # h
 	
-jal ret
+	jal ret
 
-addi $a0 $0 0x10010000 # endereco
-addi $t0 $0 45 # x
-addi $t1 $0 8 # y
-addi $a1 $0 0xffffff # cor
-addi $a2 $0 10 # b
-add $a3 $0 4 # h
+	addi $a0 $0 0x10010000 # endereco
+	addi $t0 $t5 5 # x
+	addi $t1 $t6 -1 # y
+	addi $a1 $0 0xffffff # cor
+	addi $a2 $0 10 # b
+	add $a3 $0 4 # h
 	
-jal ret
+	jal ret
 
-addi $a0 $0 0x10010000 # endereco
-addi $t0 $0 52 # x
-addi $t1 $0 9 # y
-addi $a1 $0 0xffffff # cor
-addi $a2 $0 8 # b
-add $a3 $0 3 # h
+	addi $a0 $0 0x10010000 # endereco
+	addi $t0 $t5 12 # x
+	addi $t1 $t6 0 # y
+	addi $a1 $0 0xffffff # cor
+	addi $a2 $0 8 # b
+	add $a3 $0 3 # h
 	
-jal ret
+	jal ret
 
-addi $a0 $0 0x10010000 # endereco
-addi $t0 $0 48 # x
-addi $t1 $0 7 # y
-addi $a1 $0 0xffffff # cor
-addi $a2 $0 4 # b
-add $a3 $0 4 # h
+	addi $a0 $0 0x10010000 # endereco
+	addi $t0 $t5 8 # x
+	addi $t1 $t6 -2 # y
+	addi $a1 $0 0xffffff # cor
+	addi $a2 $0 4 # b
+	add $a3 $0 4 # h
 	
-jal ret
-
-addi $a0 $0 0x10010000 # endereco
-addi $t0 $0 40 # x
-addi $t1 $0 9 # y
-addi $a1 $0 0xffffff # cor
-addi $a2 $0 7 # b
-add $a3 $0 2 # h
+	jal ret
 	
-jal ret
+	addi $sp $sp 4
+	lw $ra 0($sp)
+	jr $ra
 	
-j circulos
+	
 
 # funcao para desenhar retangulos
 ret:	
-sll $t0 $t0 2 
-add $t0 $t0 $a0 # inicio + x
-sll $t1 $t1 9
-add $t0 $t0 $t1 # inicio + y
-add $a0 $0 $t0 # endereco
-add $t0 $0 $a0 
-add $t1 $0 $a1 # cor
-add $t2 $0 $a2 # b
-add $t3 $0 $a3 # h
+	sll $t0 $t0 2 
+	add $t0 $t0 $a0 # inicio + x
+	sll $t1 $t1 9
+	add $t0 $t0 $t1 # inicio + y
+	add $a0 $0 $t0 # endereco
+	add $t0 $0 $a0 
+	add $t1 $0 $a1 # cor
+	add $t2 $0 $a2 # b
+	add $t3 $0 $a3 # h
 
 laco1:	
-beq $t3 $0 fimret
+	beq $t3 $0 fimret
 laco2:	
-beq $t2 $0 fimlaco2
+	beq $t2 $0 fimlaco2
 	
-sw $t1 0($t0) # 'pinta' pixel
-addi $t0 $t0 4 # proximo endereco de memoria
+	sw $t1 0($t0) # 'pinta' pixel
+	addi $t0 $t0 4 # proximo endereco de memoria
 
-addi $t2 $t2 -1 # x--
-j laco2
+	addi $t2 $t2 -1 # x--
+	j laco2
 
 fimlaco2:
-addi $t0 $t0 512 # proxima linha 
-sll $t7 $a2 2 # x * 4 (endereco de mem)
-sub $t0 $t0 $t7 # volta para inicio
-add $t2 $0 $a2 # x = b
-addi $t3 $t3 -1 # y--
-j laco1
+	addi $t0 $t0 512 # proxima linha 
+	sll $t7 $a2 2 # x * 4 (endereco de mem)
+	sub $t0 $t0 $t7 # volta para inicio
+	add $t2 $0 $a2 # x = b
+	addi $t3 $t3 -1 # y--
+	j laco1
 
 fimret:	 
-jr $ra # retorno da funcao
+	jr $ra # retorno da funcao
 
-j end
+	j end
 
 # fim
 
 end:
-addi $v0 $0 10
-syscall
+	addi $v0 $0 10
+	syscall
 
--- joguinho/elipse.asm
+++ joguinho/elipse.asm
@@ -2,157 +2,195 @@
 .text
 main:
 # propriedades graficas
-addi $t0 $0 512 # width tela
-addi $t1 $0 256 # height tela
-addi $t2 $0 4 # proporcao pixels
-
+	addi $t0 $0 512 # width tela
+	addi $t1 $0 256 # height tela
+	addi $t2 $0 4 # proporcao pixels
+#ajuste de proporcao
+	mul $t0 $t0 $t2
+	mul $t1 $t1 $t2
+	
+# teste de resolucao	
+	addi $at $0 0
+	beq $at $0 continue
+	
+	add $t3 $0 $t0 # w
+	add $t4 $0 $t1 # h
+	addi $t5 $0 0x10010000
+	addi $t6 $0 0x000000
+	
+s1: 	
+	beq $t3 $0 fims1
+s2:	
+	beq $t4 $0 fims2 
+	sw $t6 0($t5)
+	addi $t4 $t4 -1
+	addi $t5 $t5 4
+	j s2
+fims2:	
+	addi $t3 $t3 -1
+	add $t4 $0 $t1
+	andi $t7 $t3 1
+	bne $t7 $0 imp
+	addi $t6 $t6 0x010101
+imp:
+	j s1
+fims1:
+	addi $t5 $t5 -4
+	add $t6 $0 0xff0000
+	sw $t6 0($t5)
+	j end
+	
+continue:	
+	
+	
 # parametros da funcao circ1
-lui $a0 0x1001 # endereco inicial do centro
+	lui $a0 0x1001 # endereco inicial do centro
 
-addi $t3 $0 60 # x (deslocamento no eixo x a partir do inicio)
-addi $t4 $0 5 # y (deslocamento no eixo y a partir do inicio)
-sll $t5 $t3 2 # x * 4 (x memorias) 
-add $a0 $a0 $t5 # deslocamento do inicio por x
-mul $t5 $t4 $t0 # y * width (512)
-add $a0 $a0 $t5 # deslocamento do inicio por y
+	addi $t3 $0 150 # x (deslocamento no eixo x a partir do inicio)
+	addi $t4 $0 150 # y (deslocamento no eixo y a partir do inicio)
+	sll $t5 $t3 2 # x * 4 (x memorias) 
+	add $a0 $a0 $t5 # deslocamento do inicio por x
+	mul $t5 $t4 $t0 # y * width
+	add $a0 $a0 $t5 # deslocamento do inicio por y
 
-addi $a1 $0 0xff # cor do circulo
-addi $a2 $0 3 # raio do circulo
+	addi $a1 $0 0xff0000 # cor do circulo
+	addi $a2 $0 150 # raio do circulo
 
-jal circ # chama funcao circ
+	jal elip # chama funcao circ
 
 # parametros da funcao circ2
-lui $a0 0x1001 # endereco inicial do centro
+	lui $a0 0x1001 # endereco inicial do centro
 
-addi $t3 $0 63 # x (deslocamento no eixo x a partir do inicio)
-addi $t4 $0 31 # y (deslocamento no eixo y a partir do inicio)
-sll $t5 $t3 2 # x * 4 (x memorias) 
-add $a0 $a0 $t5 # deslocamento do inicio por x
-mul $t5 $t4 $t0 # y * width (512)
-add $a0 $a0 $t5 # deslocamento do inicio por y
+	addi $t3 $0 70 # x (deslocamento no eixo x a partir do inicio)
+	addi $t4 $0 35 # y (deslocamento no eixo y a partir do inicio)
+	sll $t5 $t3 2 # x * 4 (x memorias) 
+	add $a0 $a0 $t5 # deslocamento do inicio por x
+	mul $t5 $t4 $t0 # y * width
+	add $a0 $a0 $t5 # deslocamento do inicio por y
 
-addi $a1 $0 0xffffff # cor do circulo
-addi $a2 $0 16 # raio do circulo
+	addi $a1 $0 0x00ff00 # cor do circulo
+	addi $a2 $0 16 # raio do circulo
 
-jal circ # chama funcao circ
+	jal elip # chama funcao circ
 
 # parametros da funcao circ3
-lui $a0 0x1001 # endereco inicial do centro
+	lui $a0 0x1001 # endereco inicial do centro
 
-addi $t3 $0 15 # x (deslocamento no eixo x a partir do inicio)
-addi $t4 $0 15 # y (deslocamento no eixo y a partir do inicio)
-sll $t5 $t3 2 # x * 4 (x memorias) 
-add $a0 $a0 $t5 # deslocamento do inicio por x
-mul $t5 $t4 $t0 # y * width (512)
-add $a0 $a0 $t5 # deslocamento do inicio por y
+	addi $t3 $0 25 # x (deslocamento no eixo x a partir do inicio)
+	addi $t4 $0 15 # y (deslocamento no eixo y a partir do inicio)
+	sll $t5 $t3 2 # x * 4 (x memorias) 
+	add $a0 $a0 $t5 # deslocamento do inicio por x
+	mul $t5 $t4 $t0 # y * width
+	add $a0 $a0 $t5 # deslocamento do inicio por y
 
-addi $a1 $0 0xff0000 # cor do circulo
-addi $a2 $0 5 # raio do circulo
+	addi $a1 $0 0x0000ff # cor do circulo
+	addi $a2 $0 25 # raio do circulo
 
-jal circ # chama funcao circ
+	jal elip # chama funcao circ
 
-j end
+	j end
 
 # funcao para desenhar circulo
-circ:
-sw $ra 0($sp) # guarda retorno na pilha
-addi $sp $sp -4 # aponta para proximo na pilha
-add $s0 $0 $a0 # endereco inicial
-add $t5 $0 $a2 # r = raio
-addi $t6 $0 -1 # i = 1
+elip:
+	sw $ra 0($sp) # guarda retorno na pilha
+	addi $sp $sp -4 # aponta para proximo na pilha
+	add $s0 $0 $a0 # endereco inicial
+	add $t5 $0 $a2 # r = raio
+	addi $t6 $0 -1 # i = 1
 
 q4:
-slt $t7 $0 $t5 # r < 0? 0:1
-beq $t7 $0 fimq4 # while r >=0
-addi $s1 $0 4 # orientacao + linha 
+	slt $t7 $0 $t5 # r < 0? 0:1
+	beq $t7 $0 fimq4 # while r >=0
+	addi $s1 $0 4 # orientacao da linha 
 
-jal linha # chama funcao linha
+	jal linha # chama funcao linha
 
 
-add $t5 $t5 $t6 # r-= i
-addi $t6 $t6 -2 # i += 2
-add $s0 $s0 $t0 # prx linha
-j q4
+	add $t5 $t5 $t6 # r-= i
+	addi $t6 $t6 -2 # i += 2
+	add $s0 $s0 $t0 # prx linha
+	j q4
 fimq4:
-add $s0 $0 $a0 # endereco inicial
-add $t5 $0 $a2 # r = raio
-addi $t6 $0 1 # i = 1
-sub $s0 $s0 $t0 # linha ant
+	add $s0 $0 $a0 # endereco inicial
+	add $t5 $0 $a2 # r = raio
+	addi $t6 $0 1 # i = 1
+	sub $s0 $s0 $t0 # linha ant
 
 q1:
-slt $t7 $0 $t5 # r < 0? 0:1
-beq $t7 $0 fimq1 # while r >=0 
-addi $s1 $0 4 # orientacao + linha 
+	slt $t7 $0 $t5 # r < 0? 0:1
+	beq $t7 $0 fimq1 # while r >=0 
+	addi $s1 $0 4 # orientacao da linha 
 
-jal linha # chama funcao linha
+	jal linha # chama funcao linha
 
-add $t5 $t5 $t6 # r-= i
-addi $t6 $t6 -2 # i += 2
-sub $s0 $s0 $t0 # linha ant
-j q1
+	add $t5 $t5 $t6 # r-= i
+	addi $t6 $t6 -2 # i += 2
+	sub $s0 $s0 $t0 # linha ant
+	j q1
 fimq1:
-add $s0 $0 $a0 # endereco inicial
-add $t5 $0 $a2 # r = raio
-addi $t6 $0 1 # i = 1
-sub $s0 $s0 $t0 # linha ant
+	add $s0 $0 $a0 # endereco inicial
+	add $t5 $0 $a2 # r = raio
+	addi $t6 $0 1 # i = 1
+	sub $s0 $s0 $t0 # linha ant
 
 q2:
-slt $t7 $0 $t5 # r < 0? 0:1
-beq $t7 $0 fimq2 # while r >=0 
-addi $s1 $0 -4 # orientacao - linha 
+	slt $t7 $0 $t5 # r < 0? 0:1
+	beq $t7 $0 fimq2 # while r >=0 
+	addi $s1 $0 -4 # orientacao da linha 
 
-jal linha # chama funcao linha
+	jal linha # chama funcao linha
 
-add $t5 $t5 $t6 # r-= i
-addi $t6 $t6 -2 # i += 2
-sub $s0 $s0 $t0 # linha ant
+	add $t5 $t5 $t6 # r-= i
+	addi $t6 $t6 -2 # i += 2
+	sub $s0 $s0 $t0 # linha ant
 
-j q2
+	j q2
 fimq2:
-add $s0 $0 $a0 # endereco inicial
-add $t5 $0 $a2 # r = raio
-addi $t6 $0 1 # i = 1
-sub $s0 $s0 $t0 # linha ant
-#sub $s0 $s0 $t0 # linha ant
+	add $s0 $0 $a0 # endereco inicial
+	add $t5 $0 $a2 # r = raio
+	addi $t6 $0 1 # i = 1
+	sub $s0 $s0 $t0 # linha ant
+	#sub $s0 $s0 $t0 # linha ant
 
 q3:
-slt $t7 $0 $t5 # r < 0? 0:1
-beq $t7 $0 fimq3 # while r >=0 
-addi $s1 $0 -4 # orientacao - linha 
+	slt $t7 $0 $t5 # r < 0? 0:1
+	beq $t7 $0 fimq3 # while r >=0 
+	addi $s1 $0 -4 # orientacao da linha  
 
-jal linha # chama funcao linha
+	jal linha # chama funcao linha
 
-add $t5 $t5 $t6 # r-= i
-addi $t6 $t6 -2 # i += 2
-add $s0 $s0 $t0 # linha ant
+	add $t5 $t5 $t6 # r-= i
+	addi $t6 $t6 -2 # i += 2
+	add $s0 $s0 $t0 # linha ant
 
-j q3
+	j q3
 fimq3:
 
-addi $sp $sp 4 # apaga topo da pilha
-lw $ra 0($sp) # carrega topo da pilha
-jr $ra # retorna para end no topo pilha
+	addi $sp $sp 4 # apaga topo da pilha
+	lw $ra 0($sp) # carrega topo da pilha
+	jr $ra # retorna para end no topo pilha
 
 
 # funcao desenha linha
 linha:
-sw $ra 0($sp) # guarda retorno na pilha
-addi $sp $sp -4 # aponta para proximo na pilha
-add $t8 $0 $t5 # l = tamanho linha
-add $t9 $0 $s0 # endereco inicial
-add $s1 $0 $s1 # orientacao
+	sw $ra 0($sp) # guarda retorno na pilha
+	addi $sp $sp -4 # aponta para proximo na pilha
+	add $t8 $0 $t5 # l = tamanho linha
+	add $t9 $0 $s0 # endereco inicial
+	add $s1 $0 $s1 # orientacao
+	
 l:
-beq $t8 $0 fiml # while l > 0
-sw $a1 0($t9) # 'pinta' pixel
-# addi $a1 $a1 -4 # gradiente de cores
-add $t9 $t9 $s1 # endereço += 4
-addi $t8 $t8 -1 # l--
-j l
+	beq $t8 $0 fiml # while l > 0
+	sw $a1 0($t9) # 'pinta' pixel
+#addi $a1 $a1 4 # gradiente de cores
+	add $t9 $t9 $s1 # endereco += orientacao
+	addi $t8 $t8 -1 # l--
+	j l
 fiml:
-addi $sp $sp 4 # apaga topo da pilha
-lw $ra 0($sp) # carrega topo da pilha
-jr $ra # retorna para o end no topo da pilha
+	addi $sp $sp 4 # apaga topo da pilha
+	lw $ra 0($sp) # carrega topo da pilha
+	jr $ra # retorna para o end no topo da pilha
 
 end:
-addi $v0 $0 10
-syscall
+	addi $v0 $0 10
+	syscall
